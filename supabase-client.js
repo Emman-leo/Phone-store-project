e0cfa7c9609a93caf80cdc09131aeb1309155407
emmllampt07 <emmllampt07@gmail.com>
2026-01-08 22:18:04 +0000

    fix: improve admin security by using sessionStorage

--- supabase-client.js
+++ supabase-client.js
@@ -4,4 +4,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = 'https://iswhrxlsoltxvwhduezg.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlzd2hyeGxzb2x0eHZ3aGR1ZXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3Mjc1NDcsImV4cCI6MjA4MzMwMzU0N30.yi2zuDIq80uqEP23yrxFPnGL2RzTweFf817-GDqsugI';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: window.sessionStorage,
+  },
+});
